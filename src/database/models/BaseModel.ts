6394abc6a2cca637f4b397e04a116ef75fd50ca7
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2025-01-24 18:25:45 -0500

    fix mongoose paginate

--- src/database/models/BaseModel.ts
+++ src/database/models/BaseModel.ts
@@ -1,14 +1,13 @@
 import MongooseHelper from '@/helpers/MongooseHelper'
 import PaginationHelper from '@/helpers/PaginationHelper'
-import AggregateOpt from '@/types/mongoose/AggregateOpt'
 import { Model } from '@/types/mongoose/Model'
 import Pager from '@/types/pagination/Pager'
 import Paging from '@/types/pagination/Paging'
 import { HydratedDocument } from 'mongoose'
 
 export default class BaseModel {
-  static async paginate<T, RT>(model: Model<T>, pager: Pager, filters: any, extra?: AggregateOpt): Promise<Paging<RT>> {
-    const aggregation: any = await MongooseHelper.aggregation(model, pager, filters, extra)
+  static async paginate<T, RT>(model: Model<T>, pager: Pager, filters: any): Promise<Paging<RT>> {
+    const aggregation: any = await MongooseHelper.aggregation(model, pager, filters)
     const total_items: number = PaginationHelper.totalItems(aggregation)
     const page_count: number = PaginationHelper.pageCount(pager.page_size, total_items)
     const data: RT[] = PaginationHelper.data(aggregation)
